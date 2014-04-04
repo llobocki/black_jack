dd463a6d9fb729901485a12ee0585b2a9cddc42a
Łukasz Łobocki <l.lobocki@gmail.com>
2014-04-04 23:06:50 +0200

    unity

--- src/person.cpp
+++ src/person.cpp
@@ -1,81 +1,70 @@
-
 #include "person.h"
 #include "iostream"
 
-void use_soft_ace(bool &soft_ace, int &cards)
-{
-    if (soft_ace == false && cards < 12) {
-        cards += 10;
-        soft_ace = true;
-    };
+void use_soft_ace(bool &soft_ace, int &cards) {
+	if (soft_ace == false && cards < 12) {
+		cards += 10;
+		soft_ace = true;
+	};
 }
 
 void use_ace(bool &soft_ace, int &cards) {
-    if (soft_ace == true && cards > 21) {
-        soft_ace = false;
-        cards -= 10;
-    }
+	if (soft_ace == true && cards > 21) {
+		soft_ace = false;
+		cards -= 10;
+	}
 }
 
+Person::Person() {
+	value = 0;
 
-Person::Person()
-{
-    value = 0;
-
-    split = 0;
-    black_jack = false;
-    soft_ace = false;
+	split = 0;
+	black_jack = false;
+	soft_ace = false;
 
 //     card_counter = 0;
 }
 
-Person::~Person(){
+Person::~Person() {
 
 }
 
-
-int Person::get_cards() const
-{
-    return value;
+int Person::get_cards() const {
+	return value;
 }
 
-
-
-bool Person::get_black_jack() const
-{
-    return black_jack;
+bool Person::get_black_jack() const {
+	return black_jack;
 
 }
 
-void Person::reset()
-{
-    cards.clear();
-    value = 0;
+void Person::reset() {
+	cards.clear();
+	value = 0;
 //     two_cards = 0;
-    split = 0;
-    soft_ace = false;
-    black_jack = false;
+	split = 0;
+	soft_ace = false;
+	black_jack = false;
 }
 
-void Person::add(const Card card)
-{
+void Person::add(const Card card) {
 //    card.print(); // tylko do gry, nie do symulacji, trzeba to inaczej zaplanować
 
 //     actual += card.cards_counter();
 
-    cards.push_back(card);
+	cards.push_back(card);
 
-    value += card.get_value();
-    if (card.get_value() == 1) use_soft_ace(soft_ace,value); 		//test
+	value += card.get_value();
 
+	if (cards.size() == 2 && value == 21 && split == 0) {
+		black_jack = true;
+		soft_ace = false;
+	}
 
-    if (cards.size() == 2 && value == 21 && split == 0)
-    {
-        black_jack = true;
-    }
+	else if (card.get_value() == 1)
+		use_soft_ace(soft_ace, value); 		//test
 
-
-    use_ace(soft_ace,value);
+	use_ace(soft_ace, value);
 }
 
 // void Person::add_no_print(const Card card)
@@ -95,96 +84,78 @@ void Person::add(const Card card)
 //     use_ace(soft_ace,value);
 // }
 
+void Person::add_basic(const Card card) {
+	cards.push_back(card);
 
-void Person::add_basic(const Card card)
-{
-    cards.push_back(card);
+	value += card.get_value();
+	if (card.get_value() == 1)
+		use_soft_ace(soft_ace, value); 		//test
 
-    value += card.get_value();
-    if (card.get_value() == 1) use_soft_ace(soft_ace,value); 		//test
+	if (cards.size() == 2 && value == 21 && split == 0) {
+		black_jack = true;
+	}
 
-
-    if (cards.size() == 2 && value == 21 && split == 0)
-    {
-        black_jack = true;
-    }
-
-
-    use_ace(soft_ace,value);
+	use_ace(soft_ace, value);
 }
 
-void Person::add_hi_low_I(const Card card, int& actual)
-{
-    actual += card.cards_counter();
+void Person::add_hi_low_I(const Card card, int& actual) {
+	actual += card.cards_counter();
 
-    cards.push_back(card);
+	cards.push_back(card);
 
-    value += card.get_value();
-    if (card.get_value() == 1) use_soft_ace(soft_ace,value); 		//test
+	value += card.get_value();
+	if (card.get_value() == 1)
+		use_soft_ace(soft_ace, value); 		//test
 
+	if (cards.size() == 2 && value == 21 && split == 0) {
+		black_jack = true;
+	}
 
-    if (cards.size() == 2 && value == 21 && split == 0)
-    {
-        black_jack = true;
-    }
-
-
-    use_ace(soft_ace,value);
+	use_ace(soft_ace, value);
 
 }
 
-
-
-
-void Person::print()
-{
-    if (value < 22)
-        std::cout << value << '\n';
-    else
-        std::cout << "za dużo\n";
+void Person::print() {
+	if (value < 22)
+		std::cout << value << '\n';
+	else
+		std::cout << "za dużo\n";
 
 }
 
-bool Person::can_split()
-{
-    return (cards.size() == 2 && (cards[0].get_value() == cards[1].get_value()) && split < 4) ? true : false;
+bool Person::can_split() {
+	return (cards.size() == 2 && (cards[0].get_value() == cards[1].get_value())
+			&& split < 4) ? true : false;
 }
 
-
-int Person::get_size() const
-{
-    return cards.size();
+int Person::get_size() const {
+	return cards.size();
 }
 
-int Person::get_split() const
-{
-    return split;
+int Person::get_split() const {
+	return split;
 }
 
-void Person::set_split()
-{
-    split++;
+void Person::set_split() {
+	split++;
 }
 
-Card Person::back_card()
-{
-    Card c = cards.back();
-    value -= c.get_value();
-    cards.pop_back();
-    return c;
+Card Person::back_card() {
+	Card c = cards.back();
+	value -= c.get_value();
+	cards.pop_back();
+	return c;
 }
 
-bool Person::split_aces()
-{
-    return (cards[0].get_value() == 1 && cards[1].get_value() == 1) ? true : false;
+bool Person::split_aces() {
+	return (cards[0].get_value() == 1 && cards[1].get_value() == 1) ?
+			true : false;
 }
 
-void Person::only_21()
-{
-    black_jack = false;
+void Person::only_21() {
+	black_jack = false;
 }
 
-bool Person::ace_soft() const
-{
-    return soft_ace;
+bool Person::ace_soft() const {
+	return soft_ace;
 }
--- src/person_test.cpp
+++ src/person_test.cpp
@@ -14,7 +14,7 @@ SUITE(Person) {
 TEST(negative) {
 	CHECK(false);
 }
-
+//test konstruktora Person
 TEST(Person) {
 	Person *p = new Person();
 	CHECK(p != nullptr);
@@ -25,7 +25,7 @@ TEST(Person) {
 	CHECK_EQUAL(false, p->ace_soft());
 	CHECK_EQUAL(0, p->get_size());
 }
-
+//dodanie jednej karty
 TEST(one_Card) {
 	for (int i = 1; i < 5; ++i)
 		for (int j = 1; j < 14; ++j) {
@@ -46,7 +46,7 @@ TEST(one_Card) {
 		}
 
 }
-
+//dodanie jednej karty + reset
 TEST(one_Card_reset) {
 	Person p = Person();
 	for (int i = 1; i < 5; ++i)
@@ -58,6 +58,8 @@ TEST(one_Card_reset) {
 
 			CHECK_EQUAL([](int x) {return x == 1 ? 11 : x;}(c.get_value()),
 					p.get_cards());
+//			CHECK_EQUAL(c.get_value(),
+//					p.get_cards());
 			CHECK_EQUAL(0, p.get_split());
 			CHECK_EQUAL(false, p.get_black_jack());
 			CHECK_EQUAL(
@@ -70,6 +72,58 @@ TEST(one_Card_reset) {
 		}
 
 }
+//dodanie dwóch takich samych kart
+TEST(two_Card_reset) {
+	Person p = Person();
+//	for (int i = 1; k < )
+	for (int i = 1; i < 5; ++i)
+		for (int j = 1; j < 14; ++j) {
+
+			Card c = Card(Card::Color(i), Card::Value(j));
+
+			p.add(c);
+			p.add(c);
+
+			CHECK_EQUAL([](int x) {return x == 1 ? 12 : x*2;}(c.get_value()),
+					p.get_cards());
+			CHECK_EQUAL(0, p.get_split());
+			CHECK_EQUAL(false, p.get_black_jack());
+			CHECK_EQUAL(
+					[](int x) {return x == 1 ? true : false;}(
+							c.get_value()), p.ace_soft());
+			CHECK_EQUAL(2, p.get_size());
+
+			p.reset();
+
+		}
+
+}
+//dodanie karty + asa
+TEST(ace_Card) {
+	Person p = Person();
+	for (int i = 1; i < 5; ++i)
+		for (int j = 1; j < 14; ++j) {
+
+			Card c = Card(Card::Color(1), Card::Value(1));
+			p.add(c);
+
+			c = Card(Card::Color(i), Card::Value(j));
+			p.add(c);
+
+			CHECK_EQUAL([](int x) {return x == 1 ? 12 : x+11;}(c.get_value()),
+					p.get_cards());
+			CHECK_EQUAL(0, p.get_split());
+			CHECK_EQUAL([](int x) {return x == 10 ? true : false;}(c.get_value()),
+					p.get_black_jack());
+			CHECK_EQUAL(
+					[](int x) {return x == 10 ? false : true;}(
+							c.get_value()), p.ace_soft());
+			CHECK_EQUAL(2, p.get_size());
+
+			p.reset();
+
+		}
 
 }
 
+}

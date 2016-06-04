27de27a50732852faea4d682ac179cf5ad9acefa
Łukasz Łobocki <l.lobocki@gmail.com>
2016-06-04 21:14:14 +0200

    cmake

--- src/Box.cpp
+++ src/Box.cpp
@@ -2,107 +2,94 @@
 
 #include "iostream"
 Box::Box() {
-	_bet = 0;
-	_value = 0;
-	_ace = false;
-	_another_aces = false;
-	_soft_ace = false;
-	_split = false;
-	// TODO Auto-generated constructor stub
-
+  _bet = 0;
+  _value = 0;
+  _ace = false;
+  _another_aces = false;
+  _soft_ace = false;
+  _split = false;
+  // TODO Auto-generated constructor stub
 }
 
 Box::~Box() {
-	// TODO Auto-generated destructor stub
+  // TODO Auto-generated destructor stub
 }
 
+int Box::card_value(int value) const { return value < 10 ? value : 10; }
 
-int Box::card_value(int value) const{
-	return value < 10 ? value : 10;
-}
-
-
-int Box::get_bet() const {
-	return _bet;
-}
+int Box::get_bet() const { return _bet; }
 
 int Box::get_value() const {
-	if (soft_ace())
-		return _value + 10;
-	else
-		return _value;
+  if (soft_ace())
+    return _value + 10;
+  else
+    return _value;
 }
 
-int Box::size_box() const {
-	return _box.size();
-}
+int Box::size_box() const { return _box.size(); }
 
-bool Box::split() const{
-	return _split;
-}
+bool Box::split() const { return _split; }
 
 Box::Box(const int bet) {
-	_bet = bet;
-	_value = 0;
-	_ace = false;
-	_another_aces = false;
-	_soft_ace = false;
-	_split = false;
+  _bet = bet;
+  _value = 0;
+  _ace = false;
+  _another_aces = false;
+  _soft_ace = false;
+  _split = false;
 }
 
 // dopisanie splita asów
 Box::Box(const int bet, const Card card, const bool split) {
-	_box.push(card);
-	_bet = bet;
-	_value = card_value(card.get_value());
-	_ace = false;
-	_soft_ace = false;
-	_another_aces = false;
-	_split = split;
+  _box.push(card);
+  _bet = bet;
+  _value = card_value(card.get_value());
+  _ace = false;
+  _soft_ace = false;
+  _another_aces = false;
+  _split = split;
 }
 
 void Box::card(const Card card, int bet) {
-	if (bet > 0)
-		_bet += bet;
-	_box.push(card);
-	if (card.get_value() == 1) {
-		if (_ace == false){
-			_ace = true;
-			_soft_ace = true;
-			_value += card_value(card.get_value());
-		}
-		else if (_another_aces == false){
-			_another_aces = true;
-		}
+  if (bet > 0)
+    _bet += bet;
+  _box.push(card);
+  if (card.get_value() == 1) {
+    if (_ace == false) {
+      _ace = true;
+      _soft_ace = true;
+      _value += card_value(card.get_value());
+    } else if (_another_aces == false) {
+      _another_aces = true;
+    }
 
-	}
-	// if (_soft_ace == true && _value + card_value(card.get_value()) > 21){
-	// 	_value -= 10;
-	// 	_soft_ace = false;
-	// }
-	else
-		_value += card_value(card.get_value());
+  }
+  // if (_soft_ace == true && _value + card_value(card.get_value()) > 21){
+  // 	_value -= 10;
+  // 	_soft_ace = false;
+  // }
+  else
+    _value += card_value(card.get_value());
 }
 
 bool Box::black_jack() const {
-	return _box.size() == 2 && _value == 11 && _ace == true ? true : false;
+  return _box.size() == 2 && _value == 11 && _ace == true ? true : false;
 }
 
-bool Box::can_split(int split_counter) const{
-	return (split_counter < 3 ) && _value % card_value((_box.top()).get_value()) == 0 ? true : false;
+bool Box::can_split(int split_counter) const {
+  return (split_counter < 3) &&
+                 _value % card_value((_box.top()).get_value()) == 0
+             ? true
+             : false;
 }
 
-Card Box::split_card(){
-	Card card = _box.top();
-	_box.pop();
-	_value /= 2;
-	return card;
+Card Box::split_card() {
+  Card card = _box.top();
+  _box.pop();
+  _value /= 2;
+  return card;
 }
 
-void Box::set_split(){
-	_split = true;
-}
+void Box::set_split() { _split = true; }
 
-bool Box::soft_ace() const{
-	return _soft_ace;
-}
+bool Box::soft_ace() const { return _soft_ace; }
